b08ed94d3ee4c7dab03fd96865d9c0c262b86864
Wilson Ludeña <cornejo758@gmail.com>
2022-03-13 12:03:09 -0500

    actualizando todo

--- src/config/firebaseConfig.jsx
+++ src/config/firebaseConfig.jsx
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 
 import firebase from 'firebase/compat/app';
-import getAuth from 'firebase/compat/auth';
+import 'firebase/compat/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,5 +20,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-const auth = getAuth(app);
+const auth = firebase.auth(app);
 export {app,googleAuthProvider,auth}
